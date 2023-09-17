bb97a8b477596bf33926df164dc3f7b245f8e6da
SomeoneWithOptions <andrescastesol@gmail.com>
2023-09-17 10:00:35 -0500

    added holiday struct

---
+++ holiday/holiday.go
@@ -0,0 +1,27 @@
+package holiday
+
+import "fmt"
+
+type NextHoliday struct {
+	Holiday Holiday
+	IsToday bool
+	DaysUntil int32
+}
+
+type Holiday struct {
+	Name string
+	Date string
+}
+
+func NewNextHoliday (name string ,date string ,is_today bool ,days_until int32) NextHoliday {
+		var next_holiday NextHoliday
+		next_holiday.Holiday.Name = name
+		next_holiday.Holiday.Date = date
+		next_holiday.IsToday = is_today
+		next_holiday.DaysUntil = days_until
+		return next_holiday
+}
+
+func (n NextHoliday) Print() string {
+	return fmt.Sprintf("name: %s\ndate: %s\nisToday: %v\ndaysUntil: %d", n.Holiday.Name, n.Holiday.Date, n.IsToday, n.DaysUntil)
+} 
--- main.go
+++ main.go
@@ -5,20 +5,25 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/SomeoneWithOptions/api.diafestivo.co/holiday"
 )
 
 type Message struct {
-    Text string `json:"message"`
+	Text string `json:"message"`
 }
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        message := Message{Text: "hello"}
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(message)
-				current_time := time.Now()
-				fmt.Println(current_time)
-    })
+	PORT := "3002"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		message := Message{Text: "hello"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(message)
+		current_time := time.Now()
+		fmt.Println(current_time)
+	})
 
-    http.ListenAndServe(":3002", nil)
+	cristmas_holiday := holiday.NewNextHoliday("Christmas", "31/12/2023", false, 200)
+	fmt.Println(cristmas_holiday.Print())
+	http.ListenAndServe(":"+PORT, nil)
 }
