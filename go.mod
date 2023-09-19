b5a36ce22800e8a6aafee08adeb4350cca92c8c5
SomeoneWithOptions <andrescastesol@gmail.com>
2023-09-18 19:12:37 -0500

    added env PORT

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SomeoneWithOptions/api.diafestivo.co
 
 go 1.21
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 type Message struct {
@@ -12,13 +15,22 @@ type Message struct {
 }
 
 func main() {
-	PORT := "3002"
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Error loading .env file: %v", err)
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "3002"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		message := Message{Text: "hello"}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(message)
-		current_time := time.Now()
-		fmt.Println(current_time)
+		time_iso := time.Now().Format("2006-01-02")
+		fmt.Println(time_iso)
 	})
 
 	fmt.Printf("listening on %s\n", PORT)
